76cebabac3f5232a55ecbe73a03cec6380d4b099
madz91 <34400068+madz91@users.noreply.github.com>
2017-12-12 00:32:59 +0100

    juhu der sscheiß popo ist da

--- js/animtest.js
+++ js/animtest.js
@@ -7,16 +7,19 @@ init();
 animate();
 
 function init() {
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 1;
 
     scene = new THREE.Scene();
 
-    geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-    material = new THREE.MeshNormalMaterial();
-
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    var loader = new THREE.JSONLoader();
+    loader.load('./blenderfiles/popo.json',handle_load);
+    
+    function handle_load(geometry, materials){
+        mesh = new THREE.Mesh(geometry,materials);
+        scene.add(mesh);
+        mesh.position.z =-10;
+    }
 
     // var loader = new THREE.JSONLoader();
     // loader.load('./blenderfiles/marmelab.json', function(geometry) {
@@ -44,6 +47,7 @@ function animate() {
 
     mesh.rotation.x += parseInt(app.a) / 1000;
     mesh.rotation.y += parseInt(app.b) / 1000;
+    mesh.rotation.z += parseInt(app.d) / 1000;
 
     renderer.render( scene, camera );
 

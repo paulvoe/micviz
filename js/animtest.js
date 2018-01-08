89bacb4bf735ed157c05edaf0181c0a54821ea96
madz91 <34400068+madz91@users.noreply.github.com>
2018-01-08 01:32:17 +0100

    also dat wird nix
    ausgeklammert rotationen; versuch: hintergrundfarbe ändern, und versuch lichter hinzuzufügen beides kläglich gescheitert

--- js/animtest.js
+++ js/animtest.js
@@ -3,19 +3,37 @@ var camera, scene, renderer;
 var geometry, material, mesh;
 var model;
 
+
 init();
 animate();
 
 function init() {
+    // Hintergrundfarbe sollte rot sein...
+    // scene = new THREE.Scene(); 
+    //scene.background = new THREE.Color(0x996699, 0);
+    //renderer.setClearColorHex( 0x996699, 0 );
+    //renderer.setClearColor( 0x996699, 0);
+    //setClearColor( 0x996699, 0);
+    //scene.background = new THREE.Color( 0x996699, 0);
+    
+    /* versuch von Lichtern um Oberfläche kenntlicher 
+    zu machen. */
+    var light = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(light);
+    var light2 = new THREE.PointLight(0xffffff, 0.5);
+    scene.add(light2); 
+    
+    
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 1;
 
     scene = new THREE.Scene();
 
     var loader = new THREE.JSONLoader();
-    loader.load('./blenderfiles/popo.json',handle_load);
+    loader.load('./blenderfiles/boy.json',handle_load);
     
     function handle_load(geometry, materials){
+        material = new THREE.MeshNormalMaterial();
         mesh = new THREE.Mesh(geometry,materials);
         scene.add(mesh);
         mesh.position.z =-10;
@@ -37,10 +55,10 @@ function animate() {
 
     requestAnimationFrame( animate );
 
-    mesh.rotation.x += parseInt(app.a) / 1000;
+   /* mesh.rotation.x += parseInt(app.a) / 1000;
     mesh.rotation.y += parseInt(app.b) / 1000;
-    mesh.rotation.z += parseInt(app.d) / 1000;
+    mesh.rotation.z += parseInt(app.d) / 1000; */
 
     renderer.render( scene, camera );
 
-}
+} 
